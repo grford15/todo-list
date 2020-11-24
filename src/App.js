8c1bf01687f6962c490635589db6c2f71c930674
Greg <g.rutherford10@gmail.com>
2020-11-24 17:00:25 +0000

    change app to class component & add store for localStorage

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
+import React from "react";
+import store from 'store';
 import "./App.css";
 
-function App() {
-	return (
-		<div className="app-container">
-			<div className="title-section">
-				<h2>#toDoList</h2>
-			</div>
-			<div className="task-section">
-				<input type="text" placeholder="Enter a Task" id="task-input" />
-				<button id="task-btn">Add</button>
-			</div>
-			<div className="task-list">
+class App extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state={
+			tasks: []
+		}
+		this.submitTask = this.submitTask.bind(this);
+	}
 
+	submitTask(){
+		store.set('task0', 'take the dog a walk');
+		store.set('task1', 'do some coding')
+	}
+
+	render() {
+		return (
+			<div className="app-container">
+				<div className="title-section">
+					<h2>#toDoList</h2>
+				</div>
+				<div className="task-section">
+					<input type="text" placeholder="Enter a Task" id="task-input" />
+					<button id="task-btn" onClick={this.submitTask}>Add</button>
+				</div>
+				<div className="task-list"></div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
 
 export default App;
