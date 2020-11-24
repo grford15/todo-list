af8e2382d1449eae96e2595a816ccb4f1decdb2b
Greg <g.rutherford10@gmail.com>
2020-11-24 11:07:41 +0000

    add initial input & button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="app-container">
-      <h2>#toDoList</h2>
-    </div>
-  );
+	return (
+		<div className="app-container">
+			<div className="title-section">
+				<h2>#toDoList</h2>
+			</div>
+			<div className="task-section">
+				<input type="text" placeholder="Enter a Task" id="task-input" />
+				<button id="task-btn">Add</button>
+			</div>
+			<div className="task-list">
+
+			</div>
+		</div>
+	);
 }
 
 export default App;
