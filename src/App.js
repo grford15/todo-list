5e8f15e31ed311648cbda8c80def2d3c0a356575
Greg <g.rutherford10@gmail.com>
2020-11-28 14:10:35 +0000

    add styling to task when checked off

--- src/App.js
+++ src/App.js
@@ -3,59 +3,59 @@ import "./App.css";
 
 class App extends React.Component {
 	constructor(props) {
-		super(props)
-		this.state={
+		super(props);
+		this.state = {
 			activeTasks: [
 				{
-					"task": "walk dog",
-					"completed": false
+					task: "walk dog",
+					completed: false,
 				},
 				{
-					"task": "drink water",
-					"completed": false
-				}
+					task: "drink water",
+					completed: true,
+				},
 			],
-			completedTasks: []
-		}
+			completedTasks: [],
+		};
 		this.submitTask = this.submitTask.bind(this);
 		this.completeTask = this.completeTask.bind(this);
 		this.saveToLocal = this.saveToLocal.bind(this);
 	}
 
-
-
-	submitTask(e){
+	submitTask(e) {
 		const task = {
-			"task": document.getElementById("task-input").value,
-			"completed": false
-		}
-		this.setState({
-			activeTasks: this.state.activeTasks.concat(task)
-		},
-		this.saveToLocal
+			task: document.getElementById("task-input").value,
+			completed: false,
+		};
+		this.setState(
+			{
+				activeTasks: this.state.activeTasks.concat(task),
+			},
+			this.saveToLocal
 		);
-		
-	};
+	}
 
 	saveToLocal() {
 		const localData = this.state.activeTasks;
-		localStorage.setItem('activeTasks', JSON.stringify(localData));
+		localStorage.setItem("activeTasks", JSON.stringify(localData));
 	}
 
 	completeTask(e) {
-		const currentTask = this.state.activeTasks.find(task => task.task === e.target.name);
+		const currentTask = this.state.activeTasks.find(
+			(task) => task.task === e.target.name
+		);
 
-		const updatedTasks = this.state.activeTasks.map(item => {
-			if(item.task === currentTask.task) {
-				return {...item, completed: !item.completed};
+		const updatedTasks = this.state.activeTasks.map((item) => {
+			if (item.task === currentTask.task) {
+				return { ...item, completed: !item.completed };
 			}
 			return item;
 		});
-		this.setState({activeTasks: updatedTasks});
+		this.setState({ activeTasks: updatedTasks });
 	}
 
 	render() {
-		const {activeTasks} = this.state;
+		const { activeTasks } = this.state;
 		return (
 			<div className="app-container">
 				<div className="title-section">
@@ -68,19 +68,26 @@ class App extends React.Component {
 				</div>
 				<div className="task-section">
 					<input type="text" placeholder="Enter a Task" id="task-input" />
-					<button id="task-btn" onClick={this.submitTask}>Add</button>
+					<button id="task-btn" onClick={this.submitTask}>
+						Add
+					</button>
 				</div>
 				<div className="task-list">
 					<ul className="task-view">
-					{activeTasks.length > 0 ? 
-						activeTasks.map((task, key) => (
-							<li key={key}>
-								<input type="checkbox" name={task.task} onChange={this.completeTask}/>
-								{task.task}
-								</li>
-						))
-						: null
-					} 
+						{activeTasks.length > 0
+							? activeTasks.map((task, key) => (
+									<li key={key} className={task.completed ? "completed-task" : null}>
+										<input
+											type="checkbox"
+											name={task.task}
+											checked={task.completed}
+											onChange={this.completeTask}
+										
+										/>
+										{task.task}
+									</li>
+							  ))
+							: null}
 					</ul>
 				</div>
 			</div>
